a57ea7765e71bec921c584b20c7d96c0a7122f27
Qidraxium <LiminalKestrel@gmail.com>
2025-02-10 18:21:59 -0600

    injected nav

---
+++ staging/js_base.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("js_staging.js loaded!");
+
+    function loadNavLinks() {
+        document.getElementById("links").innerHTML = `
+            <table class="nav-table">
+                <tr>
+                    <td><a class="link-button" href="index.html?page=home">HOME</a></td>
+                    <td><a class="link-button" href="index.html?page=bird">BIRD</a></td>
+                    <td><a class="link-button" href="index.html?page=computer">COMPUTER</a></td>
+                    <td><a class="link-button" href="index.html?page=team">TEAM</a></td>
+                </tr>
+            </table>
+        `;
+    }
+
+    function loadContent() {
+        const params = new URLSearchParams(window.location.search);
+        const page = params.get("page") || "home"; // Default to home
+
+        console.log("Current page:", page);
+
+        let content = {
+            home: "You are not home.",
+            bird: "The skies are not yours.",
+            computer: "The network is not safe.",
+            team: "It is possible, but not likely."
+        };
+
+        document.getElementById("page-text").innerHTML = content[page] || content["home"];
+    }
+
+    // Inject navigation links
+    loadNavLinks();
+
+    // Load page content
+    loadContent();
+});
